084f1ae96027c331b13d0c2199b668437ca6048c
Grigoryev Pavel <Green-ono@yandex.ru>
2023-02-03 21:33:53 +0300

    add CoinServiceImpl

---
+++ src/main/java/by/grigoryev/git/service/impl/CoinServiceImpl.java
@@ -0,0 +1,29 @@
+package by.grigoryev.git.service.impl;
+
+import by.grigoryev.git.model.Coin;
+import by.grigoryev.git.repository.CoinRepository;
+import by.grigoryev.git.service.CoinService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+
+@Service
+@RequiredArgsConstructor
+public class CoinServiceImpl implements CoinService {
+
+    private final CoinRepository coinRepository;
+
+    @Override
+    public Flux<Coin> findAll() {
+        WebClient webClient = WebClient.create();
+        return webClient.get()
+                .uri("https://api.coinlore.net/api/tickers/")
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToFlux(Coin.class)
+                .flatMap(coinRepository::save)
+                .log("findAll");
+    }
+}
