82ea55b8c02c7bd59c7fc0f8171e6ca3bf2143d4
Grigoryev Pavel <Green-ono@yandex.ru>
2023-02-03 21:03:49 +0300

    add String symbol and BigDecimal price to Coin

--- src/main/java/by/grigoryev/git/model/Coin.java
+++ src/main/java/by/grigoryev/git/model/Coin.java
@@ -1,8 +1,11 @@
 package by.grigoryev.git.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
 
 @Data
 @Document(collection = "coins")
@@ -11,4 +14,8 @@ public class Coin {
     @Id
     private String id;
 
+    private String symbol;
+
+    @JsonProperty("price_usd")
+    private BigDecimal price;
 }
